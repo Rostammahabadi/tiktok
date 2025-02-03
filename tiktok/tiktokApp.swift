f9ef026f78e0a58e4cd8966aed79e0c68925372c
RostamMahabadi <RostamMahabadi@gmail.com>
2025-02-03 11:52:49 -0600

    initial commit

---
+++ tiktok/tiktokApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktokApp.swift
+//  tiktok
+//
+//  Created by Rostam on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct tiktokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
